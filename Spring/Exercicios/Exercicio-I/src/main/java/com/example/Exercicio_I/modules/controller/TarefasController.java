8fdeed419c7423a3f285d9eeb0b086090dde9a32
fgmarcal <felipegarciamarcal@gmail.com>
2024-07-22 13:35:35 -0300

    add swagger descriptions and responses

--- Spring/Exercicios/Exercicio-I/src/main/java/com/example/Exercicio_I/modules/controller/TarefasController.java
+++ Spring/Exercicios/Exercicio-I/src/main/java/com/example/Exercicio_I/modules/controller/TarefasController.java
@@ -3,6 +3,10 @@ package com.example.Exercicio_I.modules.controller;
 import com.example.Exercicio_I.modules.database.Database;
 import com.example.Exercicio_I.modules.dto.CreateTarefaDTO;
 import com.example.Exercicio_I.modules.entity.Tarefa;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +17,27 @@ public class TarefasController {
 
     Database database = new Database();
 
-    //busca todas as tarefas
     @GetMapping("/tarefas/all")
+    @Operation(description = "Lista todas as tarefas criadas")
+    @ApiResponse(responseCode = "200")
     public List<Tarefa> getListaTarefas(){
         var list = List.copyOf(database.retrieveAll());
         return ResponseEntity.ok().body(list).getBody();
     }
 
-    //cria nova tarefa
+    @Operation(description = "Cria uma nova tarefa, requer apenas descrição da mesma.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Retorna a tarefa criada")
+    })
     @PostMapping("/tarefas/create")
-    public void createTarefa(@RequestBody CreateTarefaDTO dto){
+    public ResponseEntity<Tarefa> createTarefa(@RequestBody CreateTarefaDTO dto){
         Tarefa tarefa = new Tarefa(dto.descricao());
         database.add(tarefa);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(tarefa);
     }
 
-    //atualiza status (feito ou não feito)
+    @Operation(description = "Marca uma terefa como 'Completa' ou 'não completa'")
+    @ApiResponse(responseCode = "200", description = "Ok quando a alteração foi realizada")
     @PutMapping("/tarefas/{id}")
     public void updateTarefa(@PathVariable("id") long id){
         for(Tarefa t: database.retrieveAll()){
@@ -41,7 +51,8 @@ public class TarefasController {
         }
     }
 
-    //deleta tarefa por id
+    @Operation(description = "Apaga uma tarefa")
+    @ApiResponse(responseCode = "200", description = "Tarefa apagada corretamente")
     @DeleteMapping("/tarefas/{id}")
     public void deleteTarefa(@PathVariable int id){
         for(Tarefa t: database.retrieveAll()){
